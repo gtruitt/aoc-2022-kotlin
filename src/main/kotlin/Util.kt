d082334f65271cee289a7f96f7922b16654281ec
Gabe Truitt <gabe.truitt@charthop.com>
2022-12-10 16:01:22 -0500

    fix rectangular transposition

--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+
 fun resourceLines(resourceName: String) =
     object {}::class.java.getResource(resourceName)!!.readText().lines()
 
@@ -8,6 +10,6 @@ val <T> List<T>.tail: List<T>
     get() = drop(1)
 
 fun <T> List<List<T>>.transpose(): List<List<T>> {
-    val start = IntRange(0, size.dec()).map { emptyList<T>() }
-    return fold(start) { acc, ints -> acc.zip(ints).map { it.first + listOf(it.second) } }
+    val start = IntRange(0, max(size, first().size).dec()).map { emptyList<T>() }
+    return fold(start) { acc, lst -> acc.zip(lst).map { it.first + listOf(it.second) } }
 }
--- src/test/kotlin/UtilTest.kt
+++ src/test/kotlin/UtilTest.kt
@@ -3,9 +3,23 @@ import org.junit.jupiter.api.Test
 
 class UtilTest {
     @Test
-    fun `transposes a list of lists so the nth items from each become the nth transposed list`() {
+    fun `transposes square matrix`() {
         val initial = listOf(listOf(1, 2, 3), listOf(4, 5, 6), listOf(7, 8, 9))
         val transposed = listOf(listOf(1, 4, 7), listOf(2, 5, 8), listOf(3, 6, 9))
+        initial.transpose() shouldBe transposed
+    }
+
+    @Test
+    fun `transposes x-rectangular matrix`() {
+        val initial = listOf(listOf(1, 2, 3), listOf(4, 5, 6), listOf(7, 8, 9), listOf(10, 11, 12))
+        val transposed = listOf(listOf(1, 4, 7, 10), listOf(2, 5, 8, 11), listOf(3, 6, 9, 12))
+        initial.transpose() shouldBe transposed
+    }
+
+    @Test
+    fun `transposes y-rectangular matrix`() {
+        val initial = listOf(listOf(1, 2, 3, 4), listOf(5, 6, 7, 8), listOf(9, 10, 11, 12))
+        val transposed = listOf(listOf(1, 5, 9), listOf(2, 6, 10), listOf(3, 7, 11), listOf(4, 8, 12))
         initial.transpose() shouldBe transposed
     }
 }
