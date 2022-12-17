084b01c7d777f1de9ba69e0667c7fc72c1c2b872
Gabe Truitt <gabe.truitt@charthop.com>
2022-12-16 23:42:53 -0500

    Day 08, second part

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -3,6 +3,7 @@
 package day08
 
 import head
+import tail
 
 fun readForest(input: List<String>) =
     input
@@ -21,14 +22,46 @@ fun isVisible(x: Int, y: Int, forest: List<List<Int>>) =
     || (0..y.dec()).none { forest[x][it] >= forest[x][y] }
     || (y.inc()..forest.head.size.dec()).none { forest[x][it] >= forest[x][y] }
 
-fun visibleTreeCount(input: List<String>): Int {
-    val forest = readForest(input)
-    return (0..forest.size.dec())
-        .flatMap { x ->
-            (0..forest.head.size.dec())
-                .map { y ->
-                    isVisible(x, y, forest)
-                }
+fun visibleTreeCount(input: List<String>) =
+    readForest(input).let { forest ->
+        (forest.indices).flatMap { x ->
+            (forest.head.indices).map { y ->
+                isVisible(x, y, forest)
+            }
+        }.count { it }
+    }
+
+tailrec fun visibleTreeCount(myHeight: Int, trees: List<Int>, count: Int = 0): Int =
+    when {
+        trees.isEmpty() -> count
+        trees.head >= myHeight -> count.inc()
+        else -> visibleTreeCount(myHeight, trees.tail, count.inc())
+    }
+
+fun toTheLeft(x: Int, y: Int, forest: List<List<Int>>) =
+    (x.dec() downTo 0).map { forest[it][y] }
+
+fun toTheRight(x: Int, y: Int, forest: List<List<Int>>) =
+    (x.inc() until forest.size).map { forest[it][y] }
+
+fun toTheTop(x: Int, y: Int, forest: List<List<Int>>) =
+    (y.inc() until forest.head.size).map { forest[x][it] }
+
+fun toTheBottom(x: Int, y: Int, forest: List<List<Int>>) =
+    (y.dec() downTo 0).map { forest[x][it] }
+
+fun scenicScore(x: Int, y: Int, forest: List<List<Int>>) =
+    listOf(::toTheLeft, ::toTheRight, ::toTheTop, ::toTheBottom)
+        .map {
+            visibleTreeCount(forest[x][y], it(x, y, forest))
         }
-        .count { it }
-}
+        .reduce { acc, it -> acc * it }
+
+fun maxScenicScore(input: List<String>) =
+    readForest(input).let { forest ->
+        (forest.indices).flatMap { x ->
+            (forest.head.indices).map { y ->
+                scenicScore(x, y, forest)
+            }
+        }.max()
+    }
--- src/test/kotlin/Day08Test.kt
+++ src/test/kotlin/Day08Test.kt
@@ -1,4 +1,5 @@
 import day08.visibleTreeCount
+import day08.maxScenicScore
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 
@@ -6,5 +7,10 @@ class Day08Test {
     @Test
     fun `counts trees visible from outside the forest`() {
         visibleTreeCount(resourceLines("Day08.txt")) shouldBe 1812
+    }
+
+    @Test
+    fun `determines max scenic score of any tree in the forest`() {
+        maxScenicScore(resourceLines("Day08.txt")) shouldBe 315495
     }
 }
